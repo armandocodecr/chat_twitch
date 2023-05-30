e8fa7199f06195fcec1ace93638073db733ffb9d
ArmandoCode <totovip1464@gmail.com>
2023-05-29 18:33:12 -0600

    Utilizo variables de entorno para los headers

--- src/app/services/getUserImage.ts
+++ src/app/services/getUserImage.ts
@@ -1,19 +1,18 @@
 
-interface IParams{
-    Authorization: string;
-    "Client-Id": string;
-    [key: string]: string;
-}
-
 export const getUserImage = ( username: string ): Promise<string> => {
 
     const url = `https://api.twitch.tv/helix/users?login=${username}`
 
+    const auth: string = String(process.env.NEXT_PUBLIC_CLIENT_AUTHORIZATION_TWITCH)
+    const client_id: string = String(process.env.NEXT_PUBLIC_CLIENT_ID_TWITCH)
+
+    console.log({ auth, client_id })
+
     return fetch(url, {
         method: 'GET',
         headers: {
-            Authorization: 'Bearer wev7pbp5la2l815c5obo8atvk8h125',
-            "Client-Id": 'd90ayphabs6vkwrns8h21i7td47jum'
+            Authorization: `Bearer ${auth}`,
+            "Client-Id": client_id
         }
     })
         .then(response => response.json())
